d01eb1b878984f35edd22520d52d90bea9cae08b
Brandon Hogue <brandon.s.hogue@gmail.com>
2023-08-01 19:38:21 -0500

    Initial

--- image_generator.py
+++ image_generator.py
@@ -1,8 +1,4 @@
 # image_generator.py
-import requests
-import io
-import base64
-from PIL import Image, PngImagePlugin
 import webuiapi
 
 class ImageGenerator:
@@ -10,5 +6,5 @@ class ImageGenerator:
         self.api = webuiapi.WebUIApi()
 
     async def generate_image(self, prompt):
-        result = await self.api.txt2img(prompt=prompt,steps=20,sampler_index='euler',negative_prompt="text, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face", use_async=True)
+        result = await self.api.txt2img(prompt=prompt,restore_faces=True,steps=25,width=512,height=1024,sampler_index='euler',negative_prompt="text, ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face", use_async=True)
         return result.image
